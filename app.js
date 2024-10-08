e12493253b55dd0774ee764681488482b6232cec
oscarm1990 <124219163+oscarm1990@users.noreply.github.com>
2024-10-08 17:50:26 +1100

    Update app.js

--- app.js
+++ app.js
@@ -31,5 +31,5 @@ app.post('/generate-token', (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running!`);
 });
