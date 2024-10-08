7befd4adf69cbb7c7b59198cb777b9d0f4ec3e07
oscarm1990 <124219163+oscarm1990@users.noreply.github.com>
2024-10-08 17:51:50 +1100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -28,7 +28,7 @@ document.getElementById('generateToken').addEventListener('click', async () => {
     document.getElementById('tokenOutput').value = data.token;
 
     // Generate the URL to the widget page with the JWT token and ACCOUNT_KEY for local testing
-    const constructedUrl = `http://localhost:3000/widget.html?token=${data.token}&widgetKey=${encodeURIComponent(widgetKey)}`;
+    const constructedUrl = `https://chatwidgetjwt-1.onrender.com/widget.html?token=${data.token}&widgetKey=${encodeURIComponent(widgetKey)}`;
     document.getElementById('urlOutput').value = constructedUrl; // Display the constructed URL
 
     // Update the Zendesk Widget script with the user-provided Widget Key
